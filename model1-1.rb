0660fbfd4da31341f943a5e51ff5fee5feb165bd
Jacob Martinez <jacobmarcareer@gmail.com>
2021-03-30 16:55:50 -0600

    add hash

--- model1-1.rb
+++ model1-1.rb
@@ -9,3 +9,9 @@ fiancee_age = 24
 looking_to_move = true
 
 potential_move_areas = ["Aurora", "Littleton", "Westminster"]
+
+wine_pairings = {
+  "Red wine" => "steak",
+  "Dessert wine" => "key lime pie",
+  "Sparkling wine" => "Fish"
+  }
